f2497bf43ef59356c3a73ce058650238b182dc08
fathyElgazar <gazzarDev@gmail.com>
2024-02-18 15:14:23 +0200

    Make library life

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// Getting my elements
+const bookContainer = document.querySelector('.book-container');
+const btnAdd = document.getElementById('btn-add');
+const fromContainer = document.querySelector('.form-container');
+const submit = document.getElementById('btn-submit');
+const bookName = document.getElementById('book-name');
+const bookAuthor = document.getElementById('book-author');
+const bookPages = document.getElementById('book-pages');
+const bookRead = document.getElementById('read');
+const closeForm = document.querySelector('.close-form');
+
 const myLibrary = [];
 
 // Book constructor
@@ -13,35 +24,100 @@ function Book(title, author, pages, read) {
   };
 }
 
-Book.prototype.study = function () {
-  console.log('Read the book');
+//  To toggle the read status
+Book.prototype.toggleReadStatus = function () {
+  this.read = !this.read; // Toggle the read status
 };
-
-const deepWork = new Book('Deep work', 'Cal new port', 250, 'not read yet');
-
-const egoIsTheEnemy = new Book(
-  'Ego Is The Enemy',
-  'Cal new port',
-  250,
-  'not read yet'
-);
-
-const the4HoursWorkWeek = new Book(
-  'The 4 Hours Work Week',
-  'Cal new port',
-  250,
-  'Read'
-);
-
-console.log(deepWork.info());
 
 // Add book function
 function addBookToLibrary() {
-  myLibrary.push(deepWork, egoIsTheEnemy, the4HoursWorkWeek);
+  const book = new Book(
+    bookName.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookRead.checked ? true : false
+  );
+  myLibrary.push(book);
+  displayBooks();
 }
-addBookToLibrary();
-console.log(myLibrary);
 
-for (let i = 1; i <= myLibrary.length; i++) {
-  console.table(myLibrary[i]);
+// Display books
+function displayBooks() {
+  bookContainer.innerHTML = '';
+  myLibrary.forEach((book, i) => {
+    const html = `
+    <div class="book" data-index-number="${i}">
+    <h2 class="book-name">${book.title}</h2>
+    <h3 class="author"><span>by: </span>${book.author}</h3>
+    <span class="pages">Pages: ${book.pages}</span>
+    <button class="btn btn-read btn-status">${book.read}</button>
+    <span class="remove-book">X</span>
+    </div>
+    `;
+
+    bookContainer.insertAdjacentHTML('beforeend', html);
+  });
+  deleteButtons();
+  toggleRead();
 }
+
+// Event handlers
+btnAdd.addEventListener('click', function (e) {
+  fromContainer.style.visibility = 'visible';
+  fromContainer.style.opacity = 1;
+});
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  fromContainer.style.visibility = 'hidden';
+  fromContainer.style.opacity = 0;
+  if (!bookName.value) return;
+  if (!bookAuthor.value) return;
+  if (!bookPages.value) return;
+  addBookToLibrary();
+  bookName.value = bookAuthor.value = bookPages.value = '';
+  bookRead.checked = false;
+});
+
+// toggle status
+function toggleRead() {
+  const btnStatus = document.querySelectorAll('.btn-status');
+
+  btnStatus.forEach((btn, index) => {
+    btn.addEventListener('click', (e) => {
+      myLibrary[index].toggleReadStatus();
+      btn.classList.toggle('btn-read');
+      displayBooks();
+    });
+
+    const book = myLibrary[index];
+    btn.textContent = 'Read';
+    if (book.read) {
+      btn.classList.add('btn-read');
+    } else if (!book.read) {
+      btn.classList.remove('btn-read');
+    }
+  });
+}
+
+// A little to hard
+function deleteBook(deleteBtn) {
+  const book = deleteBtn.parentElement;
+  let index = Number(book.dataset.indexNumber);
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+function deleteButtons() {
+  const btnDelete = document.querySelectorAll('.remove-book');
+  btnDelete.forEach((btn) =>
+    btn.addEventListener('click', (e) => {
+      deleteBook(btn);
+    })
+  );
+}
+
+closeForm.addEventListener('click', (e) => {
+  fromContainer.style.visibility = 'hidden';
+  fromContainer.style.opacity = 0;
+});
